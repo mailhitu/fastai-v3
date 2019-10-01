4f46e9002737da444cdd678e4b277bd0503220c3
mailhitu <mail.hitu@gmail.com>
2019-10-01 17:33:44 +0530

    Signed-off-by: mailhitu <mail.hitu@gmail.com>

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/file/d/1FcO1uZvwcwhcDuQdEFeNePiN6qJZ3a6v/view?usp=sharing'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1FcO1uZvwcwhcDuQdEFeNePiN6qJZ3a6v'
 export_file_name = 'export.pkl'
 
 classes = ['beautiful', 'sexy']
